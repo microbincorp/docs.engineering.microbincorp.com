0857e50292a5def86e26cde95f817f1ce76d793e
kjasmita <k.jasmita@microbin.io>
2025-05-13 18:06:22 +0530

    sidebar changes

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -20,12 +20,33 @@ export default defineConfig({
               { text: 'SoM', link: '/hardware/som' },
               { text: 'IO Boards', link: '/hardware/io' },
               { text: 'DVK - Development Kit', link: '/hardware/sdk' },
-              { text: 'Infinite Board Computer', link: '/hardware/sbc' },
+              { text: 'Infinite Board Computer', link: '/hardware/xbc' },
               { text: 'API Reference', link: '/hardware/api' },
               { text: 'BSP', link: '/hardware/bsp' },
+              { text: 'Drivers', link: '/hardware/drivers' },
+              {
+                text: 'Resources', collapsed: true, items: [
+                  {
+                    text: 'Datasheets',
+                    link: '/hardware/datasheets'
+                  }
+                ]
+              },
+
             ]
           },
-          { text: 'PlatformQ', link: '/platformq/' },
+          {
+            text: 'PlatformQ', link: '/platformq/', collapsed: true, items: [
+              // {
+              //   text:'Overview',
+              //   link:'/platformq/'
+              // },
+              // {
+              //   text:'Account Creation',
+              //   link:'/platformq/account-creation/'
+              // },
+            ]
+          },
           // { text: 'IoT Dashboard', link: '/iot-dashboard/' },
           // ...
         ],
---
+++ docs/hardware/xbc.md
@@ -0,0 +1,8 @@
+---
+title: Infinite Board Computers
+description: Documentation for MicroBin's Infinite Board Computer products
+---
+
+# Inifnite Board Computer
+
+MicroBin's BlackJack embedded computing board is an advanced embedded computing solution that merges the flexibility of modular architectures (SoM) with the robustness of fully integrated systems (SBC). Engineered to support multiple baseboard configurations with minimal modifications.
